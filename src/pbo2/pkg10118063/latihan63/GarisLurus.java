d2ff8512742313302f97402edef719737f7d7a8c
Naufal Asha <55681676+NaufalAshaW@users.noreply.github.com>
2019-10-23 18:27:44 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118063/latihan63/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118063.latihan63;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Asha
+ * KELAS    : IF-2
+ * NIM      : 10118063
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
